01c71bd750060121bf13fc7294b0f1a4ff56e29b
Ryan Carver <ryan@ryancarver.com>
2015-07-16 10:43:17 -0700

    feeling better about add and get

--- reactions.go
+++ reactions.go
@@ -2,6 +2,7 @@ package slack
 
 import (
 	"errors"
+	"fmt"
 	"net/url"
 )
 
@@ -18,8 +19,92 @@ type ItemReaction struct {
 	Users []string `json:"users"`
 }
 
+// ReactedToItem is an item that was reacted to, and the details of the
+// reaction.
+type ReactedToItem struct {
+	Item
+	ItemReaction
+}
+
+// AddReactionParameters is the inputs to create a new reaction.
+type AddReactionParameters struct {
+	ItemRef
+	Name string
+}
+
+// NewAddReactionParameters initialies the inputs to react to an item.
+func NewAddReactionParameters(name string, ref ItemRef) AddReactionParameters {
+	return AddReactionParameters{Name: name, ItemRef: ref}
+}
+
+// GetReactionParameters is the inputs to get reactions on an item.
+type GetReactionParameters struct {
+	ItemRef
+	Full bool
+}
+
+// NewGetReactionParameters initializes the inputs to get reactions on an item.
+func NewGetReactionParameters(ref ItemRef) GetReactionParameters {
+	return GetReactionParameters{ItemRef: ref}
+}
+
 type getReactionsResponseFull struct {
 	Message struct {
+		Type    string
+		Message struct {
+			Reactions []ItemReaction
+		}
+		File struct {
+			Reactions []ItemReaction
+		}
+		FileComment struct {
+			Comment struct {
+				Reactions []ItemReaction
+			}
+		} `json:"file_comment"`
+	}
+	SlackResponse
+}
+
+func (res getReactionsResponseFull) FindReactions() []ItemReaction {
+	switch res.Message.Type {
+	case "message":
+		return res.Message.Message.Reactions
+	case "file":
+		return res.Message.File.Reactions
+	case "file_comment":
+		return res.Message.FileComment.Comment.Reactions
+	}
+	return []ItemReaction{}
+}
+
+const (
+	DEFAULT_REACTIONS_USERID = ""
+	DEFAULT_REACTIONS_COUNT  = 100
+	DEFAULT_REACTIONS_PAGE   = 1
+	DEFAULT_REACTIONS_FULL   = false
+)
+
+// ListReactionsParameters is the inputs to find all reactions by a user.
+type ListReactionsParameters struct {
+	User  string
+	Count int
+	Page  int
+	Full  bool
+}
+
+// NewListReactionsParameters initializes the inputs to find all reactions by a user.
+func NewListReactionsParameters(userID string) ListReactionsParameters {
+	return ListReactionsParameters{
+		User:  userID,
+		Count: DEFAULT_REACTIONS_COUNT,
+		Page:  DEFAULT_REACTIONS_PAGE,
+		Full:  DEFAULT_REACTIONS_FULL,
+	}
+}
+
+type listReactionsResponseFull struct {
+	Items []struct {
 		Message struct {
 			Reactions []ItemReaction
 		}
@@ -27,70 +112,90 @@ type getReactionsResponseFull struct {
 	SlackResponse
 }
 
-func addReactionRequest(path string, values url.Values, debug bool) (*SlackResponse, error) {
-	response := &SlackResponse{}
-	err := parseResponse(path, values, response, debug)
-	if err != nil {
-		return nil, err
-	}
-	if !response.Ok {
-		return nil, errors.New(response.Error)
-	}
-	return response, nil
-}
-
-func getReactionRequest(path string, values url.Values, debug bool) (*getReactionsResponseFull, error) {
-	response := &getReactionsResponseFull{}
-	err := parseResponse(path, values, response, debug)
-	if err != nil {
-		return nil, err
-	}
-	if !response.Ok {
-		return nil, errors.New(response.Error)
-	}
-	return response, nil
-}
-
-func setupReactionItemRef(values url.Values, item ItemRef) {
-	if item.FileId != "" {
-		values.Set("file", string(item.FileId))
-	}
-	if item.FileCommentId != "" {
-		values.Set("file_comment", string(item.FileCommentId))
-	}
-	if item.ChannelId != "" {
-		values.Set("channel", string(item.ChannelId))
-	}
-	if item.Timestamp != "" {
-		values.Set("timestamp", string(item.Timestamp))
-	}
-}
-
 // AddReaction adds a reaction emoji to a message, file or file comment.
-func (api *Slack) AddReaction(name string, item ItemRef) error {
+func (api *Slack) AddReaction(params AddReactionParameters) error {
 	values := url.Values{
 		"token": {api.config.token},
 	}
-	if name != "" {
-		values.Set("name", name)
+	if params.Name != "" {
+		values.Set("name", params.Name)
 	}
-	setupReactionItemRef(values, item)
-	_, err := addReactionRequest("reactions.add", values, api.debug)
-	if err != nil {
+	if params.ChannelId != "" {
+		values.Set("channel", string(params.ChannelId))
+	}
+	if params.Timestamp != "" {
+		values.Set("timestamp", string(params.Timestamp))
+	}
+	if params.FileId != "" {
+		values.Set("file", string(params.FileId))
+	}
+	if params.FileCommentId != "" {
+		values.Set("file_comment", string(params.FileCommentId))
+	}
+	response := &SlackResponse{}
+	if err := parseResponse("reactions.add", values, response, api.debug); err != nil {
 		return err
+	}
+	if !response.Ok {
+		return errors.New(response.Error)
 	}
 	return nil
 }
 
 // GetReactions returns details about the reactions on an item.
-func (api *Slack) GetReactions(item ItemRef) ([]ItemReaction, error) {
+func (api *Slack) GetReactions(params GetReactionParameters) ([]ItemReaction, error) {
 	values := url.Values{
 		"token": {api.config.token},
 	}
-	setupReactionItemRef(values, item)
-	response, err := getReactionRequest("reactions.get", values, api.debug)
-	if err != nil {
+	if params.ChannelId != "" {
+		values.Set("channel", string(params.ChannelId))
+	}
+	if params.Timestamp != "" {
+		values.Set("timestamp", string(params.Timestamp))
+	}
+	if params.FileId != "" {
+		values.Set("file", string(params.FileId))
+	}
+	if params.FileCommentId != "" {
+		values.Set("file_comment", string(params.FileCommentId))
+	}
+	if params.Full != DEFAULT_REACTIONS_FULL {
+		values.Set("full", fmt.Sprintf("%t", params.Full))
+	}
+	response := &getReactionsResponseFull{}
+	if err := parseResponse("reactions.get", values, response, api.debug); err != nil {
 		return nil, err
 	}
-	return response.Message.Message.Reactions, nil
+	if !response.Ok {
+		return nil, errors.New(response.Error)
+	}
+	return response.FindReactions(), nil
+}
+
+// ListReactions returns information about the items a user reacted to.
+func (api *Slack) ListReactions(params ListReactionsParameters) ([]ReactedToItem, Paging, error) {
+	values := url.Values{
+		"token": {api.config.token},
+	}
+	if params.User != DEFAULT_REACTIONS_USERID {
+		values.Add("user", params.User)
+	}
+	if params.Count != DEFAULT_REACTIONS_COUNT {
+		values.Add("count", string(params.Count))
+	}
+	if params.Page != DEFAULT_REACTIONS_PAGE {
+		values.Add("count", string(params.Page))
+	}
+	if params.Full != DEFAULT_REACTIONS_FULL {
+		values.Add("count", fmt.Sprintf("%t", params.Full))
+	}
+	response := &listReactionsResponseFull{}
+	err := parseResponse("reactions.list", values, response, api.debug)
+	if err != nil {
+		return nil, Paging{}, err
+	}
+	if !response.Ok {
+		return nil, Paging{}, errors.New(response.Error)
+	}
+	return nil, Paging{}, nil
 }
--- reactions_test.go
+++ reactions_test.go
@@ -6,34 +6,110 @@ import (
 	"testing"
 )
 
+func init() {
+	http.HandleFunc("/reactions.add", addReactionHandler)
+	http.HandleFunc("/reactions.get", getReactionHandler)
+}
+
 var (
+	gotParams         map[string]string
 	addedReaction     Reaction
+	getReactionRes    string
 	gottenReactionRef ItemRef
 )
 
+func accumulateFormValue(k string, r *http.Request) {
+	if v := r.FormValue(k); v != "" {
+		gotParams[k] = v
+	}
+}
+
 func addReactionHandler(w http.ResponseWriter, r *http.Request) {
-	addedReaction.Name = r.FormValue("name")
-	addedReaction.FileId = r.FormValue("file")
-	addedReaction.FileCommentId = r.FormValue("file_comment")
-	addedReaction.ChannelId = r.FormValue("channel")
-	addedReaction.Timestamp = r.FormValue("timestamp")
+	accumulateFormValue("name", r)
+	accumulateFormValue("file", r)
+	accumulateFormValue("file_comment", r)
+	accumulateFormValue("channel", r)
+	accumulateFormValue("timestamp", r)
 	w.Header().Set("Content-Type", "application/json")
 	w.Write([]byte(`{ "ok": true }`))
 }
 
 func getReactionHandler(w http.ResponseWriter, r *http.Request) {
-	gottenReactionRef.FileId = r.FormValue("file")
-	gottenReactionRef.FileCommentId = r.FormValue("file_comment")
-	gottenReactionRef.ChannelId = r.FormValue("channel")
-	gottenReactionRef.Timestamp = r.FormValue("timestamp")
+	accumulateFormValue("file", r)
+	accumulateFormValue("file_comment", r)
+	accumulateFormValue("channel", r)
+	accumulateFormValue("timestamp", r)
+	accumulateFormValue("full", r)
 	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(`{"ok": true,
+	w.Write([]byte(getReactionRes))
+}
+
+func TestSlack_AddReaction_ToMessage(t *testing.T) {
+	once.Do(startServer)
+	SLACK_API = "http://" + serverAddr + "/"
+	api := New("testing-token")
+	wantParams := map[string]string{
+		"name":      "thumbsup",
+		"channel":   "ChannelID",
+		"timestamp": "123",
+	}
+	gotParams = map[string]string{}
+	params := NewAddReactionParameters("thumbsup", NewRefToMessage("ChannelID", "123"))
+	err := api.AddReaction(params)
+	if err != nil {
+		t.Fatalf("Unexpected error: %s", err)
+	}
+	if !reflect.DeepEqual(gotParams, wantParams) {
+		t.Errorf("Got params %#v, want %#v", gotParams, wantParams)
+	}
+}
+
+func TestSlack_AddReaction_ToFile(t *testing.T) {
+	once.Do(startServer)
+	SLACK_API = "http://" + serverAddr + "/"
+	api := New("testing-token")
+	wantParams := map[string]string{
+		"name": "thumbsup",
+		"file": "FileID",
+	}
+	gotParams = map[string]string{}
+	params := NewAddReactionParameters("thumbsup", NewRefToFile("FileID"))
+	err := api.AddReaction(params)
+	if err != nil {
+		t.Fatalf("Unexpected error: %s", err)
+	}
+	if !reflect.DeepEqual(gotParams, wantParams) {
+		t.Errorf("Got params %#v, want %#v", gotParams, wantParams)
+	}
+}
+
+func TestSlack_AddReaction_ToFileComment(t *testing.T) {
+	once.Do(startServer)
+	SLACK_API = "http://" + serverAddr + "/"
+	api := New("testing-token")
+	wantParams := map[string]string{
+		"name":         "thumbsup",
+		"file_comment": "FileCommentID",
+	}
+	gotParams = map[string]string{}
+	params := NewAddReactionParameters("thumbsup", NewRefToFileComment("FileCommentID"))
+	err := api.AddReaction(params)
+	if err != nil {
+		t.Fatalf("Unexpected error: %s", err)
+	}
+	if !reflect.DeepEqual(gotParams, wantParams) {
+		t.Errorf("Got params %#v, want %#v", gotParams, wantParams)
+	}
+}
+
+func TestSlack_GetReaction_ToMessage(t *testing.T) {
+	once.Do(startServer)
+	SLACK_API = "http://" + serverAddr + "/"
+	api := New("testing-token")
+	getReactionRes = `{"ok": true,
     "message": {
         "type": "message",
-        "channel": "C2147483705",
         "message": {
-            "type": "message",
-            "ts": "111",
             "reactions": [
                 {
                     "name": "astonished",
@@ -47,53 +123,115 @@ func getReactionHandler(w http.ResponseWriter, r *http.Request) {
                 }
             ]
         }
-    }}`))
-}
-
-func TestSlack_AddReaction(t *testing.T) {
-	http.HandleFunc("/reactions.add", addReactionHandler)
-	once.Do(startServer)
-	SLACK_API = "http://" + serverAddr + "/"
-	api := New("testing-token")
-	want := Reaction{}
-	want.Name = "thumbsup"
-	want.FileId = "FileID"
-	want.FileCommentId = "FileCommentID"
-	want.ChannelId = "ChannelID"
-	want.Timestamp = "123"
-	addedReaction = Reaction{}
-	err := api.AddReaction("thumbsup", want.ItemRef)
-	if err != nil {
-		t.Fatalf("Unexpected error: %s", err)
-	}
-	if got := addedReaction; !reflect.DeepEqual(got, want) {
-		t.Errorf("Got reaction %#v, want %#v", got, want)
-	}
-}
-
-func TestSlack_GetReaction(t *testing.T) {
-	http.HandleFunc("/reactions.get", getReactionHandler)
-	once.Do(startServer)
-	SLACK_API = "http://" + serverAddr + "/"
-	api := New("testing-token")
-	ref := ItemRef{}
-	ref.FileId = "FileID"
-	ref.FileCommentId = "FileCommentID"
-	ref.ChannelId = "ChannelID"
-	ref.Timestamp = "123"
-	gottenReactionRef = ItemRef{}
-	got, err := api.GetReactions(ref)
-	if err != nil {
-		t.Fatalf("Unexpected error: %s", err)
-	}
-	if got, want := gottenReactionRef, ref; !reflect.DeepEqual(got, want) {
-		t.Errorf("Got reaction ref %#v, want %#v", got, want)
-	}
+    }}`
 	want := []ItemReaction{
 		ItemReaction{Name: "astonished", Count: 3, Users: []string{"U1", "U2", "U3"}},
 		ItemReaction{Name: "clock1", Count: 3, Users: []string{"U1", "U2"}},
 	}
+	wantParams := map[string]string{
+		"channel":   "ChannelID",
+		"timestamp": "123",
+	}
+	gotParams = map[string]string{}
+	params := NewGetReactionParameters(NewRefToMessage("ChannelID", "123"))
+	got, err := api.GetReactions(params)
+	if err != nil {
+		t.Fatalf("Unexpected error: %s", err)
+	}
 	if !reflect.DeepEqual(got, want) {
 		t.Errorf("Got reaction %#v, want %#v", got, want)
+	}
+	if !reflect.DeepEqual(gotParams, wantParams) {
+		t.Errorf("Got params %#v, want %#v", gotParams, wantParams)
+	}
+}
+
+func TestSlack_GetReaction_ToFile(t *testing.T) {
+	once.Do(startServer)
+	SLACK_API = "http://" + serverAddr + "/"
+	api := New("testing-token")
+	getReactionRes = `{"ok": true,
+    "message": {
+        "type": "file",
+        "file": {
+            "reactions": [
+                {
+                    "name": "astonished",
+                    "count": 3,
+                    "users": [ "U1", "U2", "U3" ]
+                },
+                {
+                    "name": "clock1",
+                    "count": 3,
+                    "users": [ "U1", "U2" ]
+                }
+            ]
+        }
+    }}`
+	want := []ItemReaction{
+		ItemReaction{Name: "astonished", Count: 3, Users: []string{"U1", "U2", "U3"}},
+		ItemReaction{Name: "clock1", Count: 3, Users: []string{"U1", "U2"}},
+	}
+	wantParams := map[string]string{
+		"file": "FileID",
+		"full": "true",
+	}
+	gotParams = map[string]string{}
+	params := NewGetReactionParameters(NewRefToFile("FileID"))
+	params.Full = true
+	got, err := api.GetReactions(params)
+	if err != nil {
+		t.Fatalf("Unexpected error: %s", err)
+	}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Got reaction %#v, want %#v", got, want)
+	}
+	if !reflect.DeepEqual(gotParams, wantParams) {
+		t.Errorf("Got params %#v, want %#v", gotParams, wantParams)
+	}
+}
+
+func TestSlack_GetReaction_ToFileComment(t *testing.T) {
+	once.Do(startServer)
+	SLACK_API = "http://" + serverAddr + "/"
+	api := New("testing-token")
+	getReactionRes = `{"ok": true,
+    "message": {
+        "type": "file_comment",
+        "file_comment": {
+	    "comment": {
+                "reactions": [
+                    {
+                        "name": "astonished",
+                        "count": 3,
+                        "users": [ "U1", "U2", "U3" ]
+                    },
+                    {
+                        "name": "clock1",
+                        "count": 3,
+                        "users": [ "U1", "U2" ]
+                    }
+                ]
+            }
+        }
+    }}`
+	want := []ItemReaction{
+		ItemReaction{Name: "astonished", Count: 3, Users: []string{"U1", "U2", "U3"}},
+		ItemReaction{Name: "clock1", Count: 3, Users: []string{"U1", "U2"}},
+	}
+	wantParams := map[string]string{
+		"file_comment": "FileCommentID",
+	}
+	gotParams = map[string]string{}
+	params := NewGetReactionParameters(NewRefToFileComment("FileCommentID"))
+	got, err := api.GetReactions(params)
+	if err != nil {
+		t.Fatalf("Unexpected error: %s", err)
+	}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Got reaction %#v, want %#v", got, want)
+	}
+	if !reflect.DeepEqual(gotParams, wantParams) {
+		t.Errorf("Got params %#v, want %#v", gotParams, wantParams)
 	}
 }
