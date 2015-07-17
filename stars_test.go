602fc3388f054a85cbf85f31fcf359950b42962e
Ryan Carver <ryan@ryancarver.com>
2015-07-17 16:18:26 -0700

    refactor stars_test

--- stars_test.go
+++ stars_test.go
@@ -6,12 +6,26 @@ import (
 	"testing"
 )
 
-var starsTests = struct {
-	in        []byte
-	outItems  []StarredItem
-	outPaging *Paging
-}{
-	[]byte(`{"ok": true,
+type starsHandler struct {
+	response string
+}
+
+func (rh *starsHandler) handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(rh.response))
+}
+
+func TestSlack_GetStarred(t *testing.T) {
+	once.Do(startServer)
+	SLACK_API = "http://" + serverAddr + "/"
+	api := New("testing-token")
+	tests := []struct {
+		json         string
+		starredItems []StarredItem
+		paging       *Paging
+	}{
+		{
+			`{"ok": true,
     "items": [
         {
             "type": "message",
@@ -53,46 +67,37 @@ var starsTests = struct {
         "total": 4,
         "page": 1,
         "pages": 1
-    }}`),
-	[]StarredItem{
-		{Item: NewMessageItem("C2147483705", &Message{Msg: Msg{Text: "hello"}})},
-		{Item: NewFileItem(&File{Name: "toy"})},
-		{Item: NewFileCommentItem(&File{Name: "toy"}, &Comment{Comment: "nice"})},
-		{Item: NewChannelItem("C2147483705")},
-		{Item: NewIMItem("D1")},
-		{Item: NewGroupItem("G1")},
-	},
-	&Paging{
-		Count: 100,
-		Total: 4,
-		Page:  1,
-		Pages: 1,
-	},
-}
-
-func getStarredHandler(rw http.ResponseWriter, r *http.Request) {
-	rw.Header().Set("Content-Type", "application/json")
-	// XXX: I stripped the actual content just to test this test Oo
-	// At some point I should add valid content here
-	rw.Write(starsTests.in)
-}
-
-func TestGetStarred(t *testing.T) {
-	http.HandleFunc("/stars.list", getStarredHandler)
-	once.Do(startServer)
-	SLACK_API = "http://" + serverAddr + "/"
-	api := New("testing-token")
-	response_items, response_paging, err := api.GetStarred(NewStarsParameters())
-	if err != nil {
-		t.Errorf("Unexpected error: %s", err)
-		return
+    }}`,
+			[]StarredItem{
+				{Item: NewMessageItem("C2147483705", &Message{Msg: Msg{Text: "hello"}})},
+				{Item: NewFileItem(&File{Name: "toy"})},
+				{Item: NewFileCommentItem(&File{Name: "toy"}, &Comment{Comment: "nice"})},
+				{Item: NewChannelItem("C2147483705")},
+				{Item: NewIMItem("D1")},
+				{Item: NewGroupItem("G1")},
+			},
+			&Paging{
+				Count: 100,
+				Total: 4,
+				Page:  1,
+				Pages: 1,
+			},
+		},
 	}
-	eq := reflect.DeepEqual(response_items, starsTests.outItems)
-	if !eq {
-		t.Errorf("got %v; want %v", response_items, starsTests.outItems)
-	}
-	eq = reflect.DeepEqual(response_paging, starsTests.outPaging)
-	if !eq {
-		t.Errorf("got %v; want %v", response_paging, starsTests.outPaging)
+	var sh *starsHandler
+	http.HandleFunc("/stars.list", func(w http.ResponseWriter, r *http.Request) { sh.handler(w, r) })
+	for i, test := range tests {
+		sh = &starsHandler{}
+		sh.response = test.json
+		response_items, response_paging, err := api.GetStarred(NewStarsParameters())
+		if err != nil {
+			t.Fatalf("%d Unexpected error: %s", i, err)
+		}
+		if !reflect.DeepEqual(response_items, test.starredItems) {
+			t.Errorf("%d got %v; want %v", i, response_items, test.starredItems)
+		}
+		if !reflect.DeepEqual(response_paging, test.paging) {
+			t.Errorf("%d got %v; want %v", i, response_paging, test.paging)
+		}
 	}
 }
