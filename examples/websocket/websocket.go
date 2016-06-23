faac376828565b0d1dce05142add386de5fb7363
Pavel Dotsulenko <pavel@assembla.com>
2016-06-24 00:01:13 +0300

    Fix nil pointer dereference in websocket.go example

--- examples/websocket/websocket.go
+++ examples/websocket/websocket.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/nlopes/slack"
 )
 
 func main() {
 	api := slack.New("YOUR TOKEN HERE")
+	logger := log.New(os.Stdout, "slack-bot: ", log.Lshortfile|log.LstdFlags)
+	slack.SetLogger(logger)
 	api.SetDebug(true)
 
 	rtm := api.NewRTM()
