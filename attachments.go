73e883a161950a99fe9701f7f6b1eae29cef8ace
David Palm <dvdplm@gmail.com>
2016-06-23 15:09:35 +0200

    Add AttachmentAction and ConfirmationField types Add CallbackID, Action, Footer, FooterIcon and Ts members to Attachment

--- attachments.go
+++ attachments.go
@@ -8,10 +8,32 @@ type AttachmentField struct {
 	Short bool   `json:"short"`
 }
 
+// AttachmentAction is a button to be included in the attachment. Required when
+// using message buttons and otherwise not useful. A maximum of 5 actions may be
+// provided per attachment.
+type AttachmentAction struct {
+	Name    string              `json:"name"`              // Required.
+	Text    string              `json:"text"`              // Required.
+	Style   string              `json:"style,omitempty"`   // Optional. Allowed values: "default", "primary", "danger"
+	Type    string              `json:"type"`              // Required. Must be set to "button"
+	Value   string              `json:"value,omitempty"`   // Optional.
+	Confirm []ConfirmationField `json:"confirm,omitempty"` // Optional.
+}
+
+// ConfirmationField are used to ask users to confirm actions
+type ConfirmationField struct {
+	Title       string `json:"title,omitempty"`        // Optional.
+	Text        string `json:"text"`                   // Required.
+	OkText      string `json:"ok_text,omitempty"`      // Optional. Defaults to "Okay"
+	DismissText string `json:"dismiss_text,omitempty"` // Optional. Defaults to "Cancel"
+}
+
 // Attachment contains all the information for an attachment
 type Attachment struct {
 	Color    string `json:"color,omitempty"`
 	Fallback string `json:"fallback"`
+
+	CallbackID string `json:"callback_id,omitempty"`
 
 	AuthorName    string `json:"author_name,omitempty"`
 	AuthorSubname string `json:"author_subname,omitempty"`
@@ -26,6 +48,12 @@ type Attachment struct {
 	ImageURL string `json:"image_url,omitempty"`
 	ThumbURL string `json:"thumb_url,omitempty"`
 
-	Fields     []AttachmentField `json:"fields,omitempty"`
-	MarkdownIn []string          `json:"mrkdwn_in,omitempty"`
+	Fields     []AttachmentField  `json:"fields,omitempty"`
+	Actions    []AttachmentAction `json:"actions,omitempty"`
+	MarkdownIn []string           `json:"mrkdwn_in,omitempty"`
+
+	Footer     string `json:"footer,omitempty"`
+	FooterIcon string `json:"footer_icon,omitempty"`
+
+	Ts int64 `json:"ts,omitempty"`
 }
