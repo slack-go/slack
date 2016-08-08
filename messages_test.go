d232f6c8ed2dae89facb58e7b5114385f7abfda7
Miguel Molina <hi@mvader.me>
2016-08-08 13:42:35 +0200

    Fix tests

--- messages_test.go
+++ messages_test.go
@@ -753,3 +753,39 @@ func TestFileShareMessage(t *testing.T) {
 	assert.True(t, message.Upload)
 	assert.NotNil(t, message.File)
 }
+
+var testCallback = `{
+  "actions": [
+    {
+      "name": "recommend",
+      "value": "yes"
+    }
+  ],
+  "callback_id": "test_callback",
+  "team": {
+    "id": "T47563693",
+    "domain": "watermelonsugar"
+  },
+  "channel": {
+    "id": "C065W1189",
+    "name": "forgotten-works"
+  },
+  "user": {
+    "id": "U045VRZFT",
+    "name": "brautigan"
+  },
+  "action_ts": "1458170917.164398",
+  "message_ts": "1458170866.000004",
+  "attachment_id": "1",
+  "token": "xAB3yVzGS4BQ3O9FACTa8Ho4",
+  "original_message": "{\"text\":\"New comic book alert!\",\"attachments\":[{\"title\":\"The Further Adventures of Slackbot\",\"fields\":[{\"title\":\"Volume\",\"value\":\"1\",\"short\":true},{\"title\":\"Issue\",\"value\":\"3\",\"short\":true}],\"author_name\":\"Stanford S. Strickland\",\"author_icon\":\"https://api.slack.com/img/api/homepage_custom_integrations-2x.png\",\"image_url\":\"http://i.imgur.com/OJkaVOI.jpg?1\"},{\"title\":\"Synopsis\",\"text\":\"After @episod pushed exciting changes to a devious new branch back in Issue 1, Slackbot notifies @don about an unexpected deploy...\"},{\"fallback\":\"Would you recommend it to customers?\",\"title\":\"Would you recommend it to customers?\",\"callback_id\":\"comic_1234_xyz\",\"color\":\"#3AA3E3\",\"attachment_type\":\"default\",\"actions\":[{\"name\":\"recommend\",\"text\":\"Recommend\",\"type\":\"button\",\"value\":\"recommend\"},{\"name\":\"no\",\"text\":\"No\",\"type\":\"button\",\"value\":\"bad\"}]}]}",
+  "response_url": "https://hooks.slack.com/actions/T47563693/6204672533/x7ZLaiVMoECAW50Gw1ZYAXEM"
+}`
+
+func TestUnmarshalJSONAttachmentActionCallback(t *testing.T) {
+	var callback AttachmentActionCallback
+	err := json.Unmarshal([]byte(testCallback), &callback)
+	assert.Nil(t, err)
+	fmt.Printf("%#v", callback.OriginalMessage)
+	assert.Equal(t, "New comic book alert!", callback.OriginalMessage.Text)
+}
