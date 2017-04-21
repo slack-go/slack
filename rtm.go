46797b291f07d6b127fa21c965ce09054cdb11b0
David Palm <dvdplm@gmail.com>
2017-04-21 18:34:23 +0200

    Add ConnectRTM call to use the rtm.connect endpoint

--- rtm.go
+++ rtm.go
@@ -32,6 +32,33 @@ func (api *Client) StartRTM() (info *Info, websocketURL string, err error) {
 	return &response.Info, websocketURL, nil
 }
 
+// ConnectRTM calls the "rtm.connect" endpoint and returns the provided URL and the compact Info
+// block.
+//
+// To have a fully managed Websocket connection, use `NewRTM`, and call `ManageConnection()`
+// on it.
+func (api *Client) ConnectRTM() (info *Info, websocketURL string, slackResponse WebResponse, err error) {
+	response := &infoResponseFull{}
+	err = post("rtm.connect", url.Values{"token": {api.config.token}}, response, api.debug)
+	if err != nil {
+		return nil, "", response.WebResponse, fmt.Errorf("post: %s", err)
+	}
+	if !response.Ok {
+		return nil, "", response.WebResponse, response.Error
+	}
+
+	// websocket.Dial does not accept url without the port (yet)
+	// Fixed by: https://github.com/golang/net/commit/5058c78c3627b31e484a81463acd51c7cecc06f3
+	// but slack returns the address with no port, so we have to fix it
+	api.Debugln("Using URL:", response.Info.URL)
+	websocketURL, err = websocketizeURLPort(response.Info.URL)
+	if err != nil {
+		return nil, "", response.WebResponse, fmt.Errorf("parsing response URL: %s", err)
+	}
+
+	return &response.Info, websocketURL, response.WebResponse, nil
+}
+
 // NewRTM returns a RTM, which provides a fully managed connection to
 // Slack's websocket-based Real-Time Messaging protocol./
 func (api *Client) NewRTM() *RTM {
