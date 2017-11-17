ba0910c3ee878110266e6165428beef977730c1a
(Zenix) Han-Sheng Huang <hansheng.huang@aperza.biz>
2017-11-17 15:10:36 +0900

    add fields to user profile

--- users.go
+++ users.go
@@ -15,29 +15,33 @@ const (
 
 // UserProfile contains all the information details of a given user
 type UserProfile struct {
-	FirstName          string `json:"first_name"`
-	LastName           string `json:"last_name"`
-	RealName           string `json:"real_name"`
-	RealNameNormalized string `json:"real_name_normalized"`
-	Email              string `json:"email"`
-	Skype              string `json:"skype"`
-	Phone              string `json:"phone"`
-	Image24            string `json:"image_24"`
-	Image32            string `json:"image_32"`
-	Image48            string `json:"image_48"`
-	Image72            string `json:"image_72"`
-	Image192           string `json:"image_192"`
-	ImageOriginal      string `json:"image_original"`
-	Title              string `json:"title"`
-	BotID              string `json:"bot_id,omitempty"`
-	ApiAppID           string `json:"api_app_id,omitempty"`
-	StatusText         string `json:"status_text,omitempty"`
-	StatusEmoji        string `json:"status_emoji,omitempty"`
+	FirstName             string `json:"first_name"`
+	LastName              string `json:"last_name"`
+	RealName              string `json:"real_name"`
+	RealNameNormalized    string `json:"real_name_normalized"`
+	DisplayName           string `json:"display_name"`
+	DisplayNameNormalized string `json:"display_name_normalized"`
+	Email                 string `json:"email"`
+	Skype                 string `json:"skype"`
+	Phone                 string `json:"phone"`
+	Image24               string `json:"image_24"`
+	Image32               string `json:"image_32"`
+	Image48               string `json:"image_48"`
+	Image72               string `json:"image_72"`
+	Image192              string `json:"image_192"`
+	ImageOriginal         string `json:"image_original"`
+	Title                 string `json:"title"`
+	BotID                 string `json:"bot_id,omitempty"`
+	ApiAppID              string `json:"api_app_id,omitempty"`
+	StatusText            string `json:"status_text,omitempty"`
+	StatusEmoji           string `json:"status_emoji,omitempty"`
+	Team                  string `json:"team"`
 }
 
 // User contains all the information of a user
 type User struct {
 	ID                string      `json:"id"`
+	TeamID            string      `json:"team_id"`
 	Name              string      `json:"name"`
 	Deleted           bool        `json:"deleted"`
 	Color             string      `json:"color"`
@@ -52,6 +56,7 @@ type User struct {
 	IsPrimaryOwner    bool        `json:"is_primary_owner"`
 	IsRestricted      bool        `json:"is_restricted"`
 	IsUltraRestricted bool        `json:"is_ultra_restricted"`
+	IsAppUser         bool        `json:"is_app_user"`
 	Has2FA            bool        `json:"has_2fa"`
 	HasFiles          bool        `json:"has_files"`
 	Presence          string      `json:"presence"`
