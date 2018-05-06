36329279993863b3f3d0e11f2619fdfe22b0afe1
Adam <davidatomhernandez@gmail.com>
2018-05-06 17:28:02 -0400

    update example again

--- examples/eventsapi/events.go
+++ examples/eventsapi/events.go
@@ -18,7 +18,7 @@ func main() {
 		buf := new(bytes.Buffer)
 		buf.ReadFrom(r.Body)
 		body := buf.String()
-		eventsAPIEvent, e := slackevents.ParseEventsAPIEvent(json.RawMessage(body))
+		eventsAPIEvent, e := slackevents.ParseEventsAPIEvent(json.RawMessage(body), OptionVerifyToken(&TokenComparator{"TOKEN"}))
 		if e != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 		}
