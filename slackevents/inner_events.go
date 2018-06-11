2b50f23111ed381eec18b9a231e618d96af4b31a
Raphael SantoDomingo <Raphael.SantoDomingo@target.com>
2018-06-10 23:17:35 -0500

    fix conflict

--- slackevents/inner_events.go
+++ slackevents/inner_events.go
@@ -12,12 +12,13 @@ type EventsAPIInnerEvent struct {
 
 // AppMentionEvent is an (inner) EventsAPI subscribable event.
 type AppMentionEvent struct {
-	Type           string      `json:"type"`
-	User           string      `json:"user"`
-	Text           string      `json:"text"`
-	TimeStamp      string      `json:"ts"`
-	Channel        string      `json:"channel"`
-	EventTimeStamp json.Number `json:"event_ts"`
+	Type            string      `json:"type"`
+	User            string      `json:"user"`
+	Text            string      `json:"text"`
+	TimeStamp       string      `json:"ts"`
+	Channel         string      `json:"channel"`
+	EventTimeStamp  json.Number `json:"event_ts"`
+	ThreadTimeStamp string      `json:"thread_ts"`
 }
 
 // AppUninstalledEvent Your Slack app was uninstalled.
@@ -60,15 +61,16 @@ type sharedLinks struct {
 // if ChannelType = "mim", A message was posted in a multiparty direct message channel
 // TODO: Improve this so that it is not required to manually parse ChannelType
 type MessageEvent struct {
-	Type           string      `json:"type"`
-	Subtype        string      `json:"subtype"`
-	User           string      `json:"user"`
-	Username       string      `json:"username"`
-	Text           string      `json:"text"`
-	TimeStamp      string      `json:"ts"`
-	Channel        string      `json:"channel"`
-	ChannelType    string      `json:"channel_type"`
-	EventTimeStamp json.Number `json:"event_ts"`
+	Type            string      `json:"type"`
+	Subtype         string      `json:"subtype"`
+	User            string      `json:"user"`
+	Username        string      `json:"username"`
+	Text            string      `json:"text"`
+	TimeStamp       string      `json:"ts"`
+	Channel         string      `json:"channel"`
+	ChannelType     string      `json:"channel_type"`
+	EventTimeStamp  json.Number `json:"event_ts"`
+	ThreadTimeStamp string      `json:"thread_ts"`
 }
 
 const (
