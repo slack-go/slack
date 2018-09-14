e19a9fd23b483a96d60ea03e6cce09fc8be55df7
alyosha93 <stephenjohnsonm@gmail.com>
2018-09-15 02:27:26 +0900

    reformat hashing process

--- security.go
+++ security.go
@@ -6,45 +6,42 @@ import (
 	"encoding/hex"
 	"errors"
 	"fmt"
+	"hash"
 	"net/http"
 )
 
 // SecretsVerifier contains the information needed to verify that the request comes from Slack
 type SecretsVerifier struct {
-	slackSig    string
-	timeStamp   string
-	requestBody string
+	slackSig  string
+	timeStamp string
+	hmac      hash.Hash
 }
 
-// NewSecretsVerifierFromHeader returns a new SecretsVerifier object in exchange for an http.Header object
-func NewSecretsVerifierFromHeader(header http.Header) (SecretsVerifier, error) {
+// NewSecretsVerifier returns a SecretsVerifier object in exchange for an http.Header object and signing secret
+func NewSecretsVerifier(header http.Header, signingSecret string) (SecretsVerifier, error) {
 	if header["X-Slack-Signature"][0] == "" || header["X-Slack-Request-Timestamp"][0] == "" {
 		return SecretsVerifier{}, errors.New("headers are empty, cannot create SecretsVerifier")
 	}
 
+	hash := hmac.New(sha256.New, []byte(signingSecret))
+	hash.Write([]byte(fmt.Sprintf("v0:%s:", header["X-Slack-Request-Timestamp"][0])))
 	return SecretsVerifier{
 		slackSig:  header["X-Slack-Signature"][0],
 		timeStamp: header["X-Slack-Request-Timestamp"][0],
+		hmac:      hash,
 	}, nil
 }
 
 func (v *SecretsVerifier) Write(body []byte) (n int, err error) {
-	v.requestBody = string(body)
-	return len(body), nil
+	return v.hmac.Write(body)
 }
 
 // Ensure compares the signature sent from Slack with the actual computed hash to judge validity
 func (v SecretsVerifier) Ensure(signingSecret string) error {
-	message := fmt.Sprintf("v0:%v:%v", v.timeStamp, v.requestBody)
-
-	mac := hmac.New(sha256.New, []byte(signingSecret))
-	mac.Write([]byte(message))
-
-	actualSignature := "v0=" + string(hex.EncodeToString(mac.Sum(nil)))
-	fmt.Printf("actual: %s expected: %s", actualSignature, v.slackSig)
-	if actualSignature == v.slackSig {
+	computed := "v0=" + string(hex.EncodeToString(v.hmac.Sum(nil)))
+	if computed == v.slackSig {
 		return nil
 	}
 
-	return errors.New("invalid request")
+	return fmt.Errorf("invalid request verification token %s, expected %s", v.slackSig, computed)
 }
