71794a5c2ea847f0cfb0b5542db8eb2ba93d7539
David Frascone <codemonkey@scriptonomicon.org>
2018-10-17 12:16:17 -0600

    Added auth.revoke endpoint

---
+++ auth.go
@@ -0,0 +1,43 @@
+package slack
+
+import (
+	"context"
+	"errors"
+	"net/url"
+)
+
+// AuthRevokeResponse contains our Auth response from the auth.revoke endpoint
+type AuthRevokeResponse struct {
+	SlackResponse      // Contains the "ok", and "Error", if any
+	Revoked       bool `json:"revoked,omitempty"`
+}
+
+// authRequest sends the actual request, and unmarshals the response
+func authRequest(ctx context.Context, client HTTPRequester, path string, values url.Values, debug bool) (*AuthRevokeResponse, error) {
+	response := &AuthRevokeResponse{}
+	err := postSlackMethod(ctx, client, path, values, response, debug)
+	if err != nil {
+		return nil, err
+	}
+	if !response.Ok {
+		return nil, errors.New(response.Error)
+	}
+	return response, nil
+}
+
+// SendAuthRevoke will send a revocation for our token
+func (api *Client) SendAuthRevoke(token string) (*AuthRevokeResponse, error) {
+	return api.SendAuthRevokeContext(context.Background(), token)
+}
+
+// SendAuthRevokeContext will retrieve the satus from api.test
+func (api *Client) SendAuthRevokeContext(ctx context.Context, token string) (*AuthRevokeResponse, error) {
+	if token == "" {
+		token = api.token
+	}
+	values := url.Values{
+		"token": {token},
+	}
+
+	return authRequest(ctx, api.httpclient, "auth.revoke", values, api.debug)
+}
