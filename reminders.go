e8c7d71ebf709b10881f3a771cef0d3a775477d6
Jamie Hannaford <jamie@limetree.org>
2019-01-08 15:50:26 +0100

    update reminders API

--- reminders.go
+++ reminders.go
@@ -6,40 +6,43 @@ import (
 	"net/url"
 )
 
-// AddReminder adds a reminder for a channel or a user.
-//
-// Only one of ChannelID or UserID should be provided. Both can be omitted, in
-// which case the reminder is set for the authenticated user.
-//
-// See https://api.slack.com/methods/reminders.add (NOTE: the ability to set
-// reminders on a channel is currently undocumented but has been tested to
-// work)
-func (api *Client) AddReminder(channelID, userID, text, time string) error {
-	return api.AddReminderContext(context.Background(), channelID, userID, text, time)
-}
-
-// AddReminderContext adds a reminder for a channel or a user with a custom context.
-//
-// See AddReminder for full details.
-func (api *Client) AddReminderContext(ctx context.Context, channelID, userID, text, time string) error {
-	values := url.Values{
-		"token":   {api.token},
-		"text":    {text},
-		"time":    {time},
-		"channel": {channelID},
-	}
-	if channelID != "" {
-		values.Set("channel", channelID)
-	} else if userID != "" {
-		values.Set("user", userID)
-	}
-
+func (api *Client) addReminder(ctx context.Context, values url.Values) error {
 	response := &SlackResponse{}
-	if err := post(ctx, api.httpclient, "reminders.add", values, response, api.debug); err != nil {
+	if err := postSlackMethod(ctx, api.httpclient, "reminders.add", values, response, api); err != nil {
 		return err
 	}
 	if !response.Ok {
 		return errors.New(response.Error)
 	}
 	return nil
+}
+
+// AddChannelReminder adds a reminder for a channel with a custom context.
+//
+// See https://api.slack.com/methods/reminders.add (NOTE: the ability to set
+// reminders on a channel is currently undocumented but has been tested to
+// work)
+func (api *Client) AddChannelReminder(channelID, text, time string) error {
+	values := url.Values{
+		"token":   {api.token},
+		"text":    {text},
+		"time":    {time},
+		"channel": {channelID},
+	}
+	return api.addReminder(context.Background(), values)
+}
+
+// AddUserReminder adds a reminder for a user with a custom context.
+//
+// See https://api.slack.com/methods/reminders.add (NOTE: the ability to set
+// reminders on a channel is currently undocumented but has been tested to
+// work)
+func (api *Client) AddUserReminder(userID, text, time string) error {
+	values := url.Values{
+		"token": {api.token},
+		"text":  {text},
+		"time":  {time},
+		"user":  {userID},
+	}
+	return api.addReminder(context.Background(), values)
 }
