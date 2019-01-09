5553239389683e149648a8b243aec928759c892a
Bruno Panuto <bl.panuto@gmail.com>
2019-01-09 01:12:45 -0200

    move empty Filename check up

--- files.go
+++ files.go
@@ -223,6 +223,9 @@ func (api *Client) UploadFileContext(ctx context.Context, params FileUploadParam
 	if err != nil {
 		return nil, err
 	}
+	if params.Filename == "" {
+		return nil, fmt.Errorf("files.upload: FileUploadParameters.Filename is mandatory")
+	}
 	response := &fileResponseFull{}
 	values := url.Values{
 		"token": {api.token},
@@ -251,9 +254,6 @@ func (api *Client) UploadFileContext(ctx context.Context, params FileUploadParam
 	} else if params.File != "" {
 		err = postLocalWithMultipartResponse(ctx, api.httpclient, "files.upload", params.File, "file", values, response, api)
 	} else if params.Reader != nil {
-		if params.Filename == "" {
-			return nil, fmt.Errorf("files.upload: FileUploadParameters.Filename is mandatory when supplying a Reader option")
-		}
 		err = postWithMultipartResponse(ctx, api.httpclient, "files.upload", params.Filename, "file", values, params.Reader, response, api)
 	}
 	if err != nil {
