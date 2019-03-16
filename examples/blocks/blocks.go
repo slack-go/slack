eb54e3636e4f935fa2b0af3091590561ce943a40
Junya Ogasawara <oga.ivc.s27@gmail.com>
2019-03-16 15:13:57 +0900

    Fix misconfigured package name used in examples/blocks/blocks.go

--- examples/blocks/blocks.go
+++ examples/blocks/blocks.go
@@ -4,7 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 
-	"github.com/MattDavisRV/slack"
+	"github.com/nlopes/slack"
 )
 
 // The functions below mock the different templates slack has as examples on their website.
