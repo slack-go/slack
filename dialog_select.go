dcc826616e2c8c18f0f5b2b760485877666fb8c7
Tyler Ramer <tyaramer@gmail.com>
2019-04-12 19:53:55 -0400

    Improve NewGroupedSelectDialog
    - Add convinience function `NewDialogOptionGroup` and test for the same
    - move `optionsFromArray` and `optionsFromMap` into `selectOptions....` and into the testing, as they aren't used in the exported library whatsoever.
    - Test refractor supporting the above
    
    Authored-by: Tyler Ramer <tyaramer@gmail.com>

--- dialog_select.go
+++ dialog_select.go
@@ -55,14 +55,7 @@ func NewStaticSelectDialogInput(name, label string, options []DialogSelectOption
 }
 
 // NewGroupedSelectDialogInput creates grouped options select input for Dialogs.
-func NewGroupedSelectDialogInput(name, label string, groups map[string]map[string]string) *DialogInputSelect {
-	optionGroups := []DialogOptionGroup{}
-	for groupName, options := range groups {
-		optionGroups = append(optionGroups, DialogOptionGroup{
-			Label:   groupName,
-			Options: optionsFromMap(options),
-		})
-	}
+func NewGroupedSelectDialogInput(name, label string, options []DialogOptionGroup) *DialogInputSelect {
 	return &DialogInputSelect{
 		DialogInput: DialogInput{
 			Type:  InputTypeSelect,
@@ -70,34 +63,15 @@ func NewGroupedSelectDialogInput(name, label string, groups map[string]map[strin
 			Label: label,
 		},
 		DataSource:   DialogDataSourceStatic,
-		OptionGroups: optionGroups,
-	}
+		OptionGroups: options}
 }
 
-func optionsFromArray(options []string) []DialogSelectOption {
-	selectOptions := make([]DialogSelectOption, len(options))
-	for idx, value := range options {
-		selectOptions[idx] = DialogSelectOption{
-			Label: value,
-			Value: value,
-		}
+// NewDialogOptionGroup creates a DialogOptionGroup from several select options
+func NewDialogOptionGroup(label string, options ...DialogSelectOption) DialogOptionGroup {
+	return DialogOptionGroup{
+		Label:   label,
+		Options: options,
 	}
-	return selectOptions
-}
-
-func optionsFromMap(options map[string]string) []DialogSelectOption {
-	selectOptions := make([]DialogSelectOption, len(options))
-	idx := 0
-	var option DialogSelectOption
-	for key, value := range options {
-		option = DialogSelectOption{
-			Label: key,
-			Value: value,
-		}
-		selectOptions[idx] = option
-		idx++
-	}
-	return selectOptions
 }
 
 // NewConversationsSelect returns a `Conversations` select
