9ec48eaf0ee6f93d1ce935e41c0337501dcc8142
Rafael Garcia <rafael.garcia@clever.com>
2019-04-18 19:59:15 +0000

    add parse to webhook message

--- webhooks.go
+++ webhooks.go
@@ -9,13 +9,14 @@ import (
 )
 
 type WebhookMessage struct {
-	Username         string       `json:"username,omitempty"`
-	IconEmoji        string       `json:"icon_emoji,omitempty"`
-	IconURL          string       `json:"icon_url,omitempty"`
-	Channel          string       `json:"channel,omitempty"`
-	ThreadTimestamp  string       `json:"thread_ts,omitempty"`
-	Text             string       `json:"text,omitempty"`
-	Attachments      []Attachment `json:"attachments,omitempty"`
+	Username        string       `json:"username,omitempty"`
+	IconEmoji       string       `json:"icon_emoji,omitempty"`
+	IconURL         string       `json:"icon_url,omitempty"`
+	Channel         string       `json:"channel,omitempty"`
+	ThreadTimestamp string       `json:"thread_ts,omitempty"`
+	Text            string       `json:"text,omitempty"`
+	Attachments     []Attachment `json:"attachments,omitempty"`
+	Parse           string       `json:"parse,omitempty"`
 }
 
 func PostWebhook(url string, msg *WebhookMessage) error {
