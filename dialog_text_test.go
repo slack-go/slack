354d0ee78390e9f6db7085cb8bcff39c069832ad
alexfernandessd <alex.fernandes.s.d@gmail.com>
2019-06-10 10:22:52 -0300

    change param to optional param

--- dialog_text_test.go
+++ dialog_text_test.go
@@ -10,13 +10,11 @@ func TestNewTextInput(t *testing.T) {
 	name := "internalName"
 	label := "Human Readable"
 	value := "Pre filled text"
-	optional := true
-	textInput := NewTextInput(name, label, value, optional)
+	textInput := NewTextInput(name, label, value)
 	assert.Equal(t, InputTypeText, textInput.Type)
 	assert.Equal(t, name, textInput.Name)
 	assert.Equal(t, label, textInput.Label)
 	assert.Equal(t, value, textInput.Value)
-	assert.Equal(t, optional, textInput.Optional)
 }
 
 func TestNewTextAreaInput(t *testing.T) {
