235046eb2d23a32b36790f3c0a7d2b57e4e4f816
Vegard Løkken <vegard@loekken.org>
2019-10-21 22:16:29 +0200

    Add RTM example that responds in different ways
    Demonstrating how to respond in channels vs. threads.

---
+++ examples/websocket/respond.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/nlopes/slack"
+)
+
+func main() {
+	api := slack.New(
+		"YOUR-TOKEN-HERE",
+	)
+
+	rtm := api.NewRTM()
+	go rtm.ManageConnection()
+
+	for msg := range rtm.IncomingEvents {
+		switch ev := msg.Data.(type) {
+		case *slack.MessageEvent:
+			msg := ev.Msg
+
+			if msg.SubType != "" {
+				break // We're only handling normal messages.
+			}
+
+			// Create a response object.
+			resp := rtm.NewOutgoingMessage(fmt.Sprintf("echo %s", msg.Text), msg.Channel)
+
+			// Respond in thread if not a direct message.
+			if !strings.HasPrefix(msg.Channel, "D") {
+				resp.ThreadTimestamp = msg.Timestamp
+			}
+
+			// Respond in same thread if message came from a thread.
+			if msg.ThreadTimestamp != "" {
+				resp.ThreadTimestamp = msg.ThreadTimestamp
+			}
+
+			rtm.SendMessage(resp)
+
+		case *slack.ConnectedEvent:
+			fmt.Println("Connected to Slack")
+
+		case *slack.InvalidAuthEvent:
+			fmt.Println("Invalid token")
+			return
+		}
+	}
+}
