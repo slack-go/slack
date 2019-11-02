900741889b0bd18318f120c8f3059a98302798ee
James Lawrence <james@talla.com>
2019-11-02 14:47:48 -0400

    fix connection goodbye issue

--- slacktest/handlers.go
+++ slacktest/handlers.go
@@ -111,7 +111,8 @@ func (sts *Server) postMessageHandler(w http.ResponseWriter, r *http.Request) {
 	_, _ = w.Write([]byte(resp))
 }
 
-func rtmConnectHandler(w http.ResponseWriter, r *http.Request) {
+// RTMConnectHandler generates a valid connection
+func RTMConnectHandler(w http.ResponseWriter, r *http.Request) {
 	_, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		msg := fmt.Sprintf("Error reading body: %s", err.Error())
@@ -172,61 +173,100 @@ func rtmStartHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func (sts *Server) wsHandler(w http.ResponseWriter, r *http.Request) {
-	upgrader := websocket.Upgrader{
-		ReadBufferSize:  1024,
-		WriteBufferSize: 1024,
-		CheckOrigin: func(r *http.Request) bool {
-			return true
-		},
-	}
-	c, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		msg := fmt.Sprintf("Unable to upgrade to ws connection: %s", err.Error())
-		log.Print(msg)
-		http.Error(w, msg, http.StatusInternalServerError)
-		return
-	}
-	defer func() { _ = c.Close() }()
-	serverAddr := r.Context().Value(ServerBotHubNameContextKey).(string)
-	go handlePendingMessages(c, serverAddr)
-	for {
-		mt, messageBytes, err := c.ReadMessage()
-		if websocket.IsUnexpectedCloseError(err) {
+	Websocket(func(c *websocket.Conn) {
+		serverAddr := r.Context().Value(ServerBotHubNameContextKey).(string)
+		go handlePendingMessages(c, serverAddr)
+		for {
+			var (
+				err   error
+				m     json.RawMessage
+				mtype string
+			)
+
+			if mtype, m, err = RTMRespEventType(c); err != nil {
+				if websocket.IsUnexpectedCloseError(err) {
+					return
+				}
+
+				log.Printf("read error: %s", err.Error())
+				continue
+			}
+
+			switch mtype {
+			case "ping":
+				if err = RTMRespPong(c, m); err != nil {
+					log.Println("ping error:", err)
+				}
+			default:
+				sts.postProcessMessage(string(m), serverAddr)
+			}
+		}
+	})(w, r)
+}
+
+// Websocket handler
+func Websocket(delegate func(c *websocket.Conn)) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		upgrader := websocket.Upgrader{
+			ReadBufferSize:  1024,
+			WriteBufferSize: 1024,
+			CheckOrigin: func(r *http.Request) bool {
+				return true
+			},
+		}
+		c, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			msg := fmt.Sprintf("Unable to upgrade to ws connection: %s", err.Error())
+			log.Print(msg)
+			http.Error(w, msg, http.StatusInternalServerError)
 			return
 		}
-
-		if err != nil {
-			log.Printf("read error: %s", err.Error())
-			continue
-		}
-		message := string(messageBytes)
-		evt := &slack.Event{}
-		if err := json.Unmarshal(messageBytes, evt); err != nil {
-			log.Printf("Error unmarshalling message: %s", err.Error())
-			log.Printf("failed message: %s", string(message))
-			continue
-		}
-		if evt.Type == "ping" {
-			p := &slack.Ping{}
-			jErr := json.Unmarshal(messageBytes, p)
-			if jErr != nil {
-				log.Printf("Unable to decode ping event: %s", jErr.Error())
-				continue
-			}
-			//log.Print("responding to slack ping")
-			pong := &slack.Pong{
-				ReplyTo: p.ID,
-				Type:    "pong",
-			}
-			j, _ := json.Marshal(pong)
-			wErr := c.WriteMessage(mt, j)
-			if wErr != nil {
-				log.Printf("error writing pong back to socket: %s", wErr.Error())
-				continue
-			}
-			continue
-		} else {
-			go sts.postProcessMessage(message, serverAddr)
-		}
+		defer func() { _ = c.Close() }()
+		delegate(c)
 	}
+}
+
+// RTMServerSendGoodbye send a goodbye event
+func RTMServerSendGoodbye(c *websocket.Conn) error {
+	return c.WriteJSON(slack.Event{Type: "goodbye"})
+}
+
+// RTMRespEventType retrieve the event type from the next message
+func RTMRespEventType(c *websocket.Conn) (t string, m json.RawMessage, err error) {
+	var (
+		evt slack.Event
+	)
+
+	if err = c.ReadJSON(&m); err != nil {
+		return "", m, err
+	}
+
+	if err = json.Unmarshal(m, &evt); err != nil {
+		return "", m, err
+	}
+
+	return evt.Type, m, nil
+}
+
+// RTMRespPong decode a ping and respond with a pong event.
+func RTMRespPong(c *websocket.Conn, m json.RawMessage) (err error) {
+	var (
+		ping slack.Ping
+		pong slack.Pong
+	)
+
+	if err = json.Unmarshal(m, &ping); err != nil {
+		return err
+	}
+
+	pong = slack.Pong{
+		Type:    "pong",
+		ReplyTo: ping.ID,
+	}
+
+	if err = c.WriteJSON(pong); err != nil {
+		return err
+	}
+
+	return nil
 }
