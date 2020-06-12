0e253ddc141626fdb961dac0d7578d62e7fb0ea0
Dom Hutton <Dombo@users.noreply.github.com>
2020-06-12 11:37:07 +0300

    Update examples/eventsapi to avoid TOKEN confusion

--- examples/eventsapi/events.go
+++ examples/eventsapi/events.go
@@ -18,7 +18,7 @@ func main() {
 		buf := new(bytes.Buffer)
 		buf.ReadFrom(r.Body)
 		body := buf.String()
-		eventsAPIEvent, e := slackevents.ParseEvent(json.RawMessage(body), slackevents.OptionVerifyToken(&slackevents.TokenComparator{VerificationToken: "TOKEN"}))
+		eventsAPIEvent, e := slackevents.ParseEvent(json.RawMessage(body), slackevents.OptionVerifyToken(&slackevents.TokenComparator{VerificationToken: "VERIFICATION_TOKEN"}))
 		if e != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			return
