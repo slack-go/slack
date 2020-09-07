f4af079513134b46ce05ec14d1505659d5f89f9b
Tim Heckman <t@heckman.io>
2020-09-07 01:31:59 -0700

    Add Root field to MessageEvent to support thread_broadcast subtype
    The `thread_broadcast` subtype includes extra data in the MessageEvent that
    we're currently losing, the `root` field. This is where it puts the message that
    is being shared to a channel as a reply to a thread.
    
    Fixes #792

--- slackevents/inner_events.go
+++ slackevents/inner_events.go
@@ -115,6 +115,11 @@ type MessageEvent struct {
 	Files  []File `json:"files"`
 
 	Attachments []slack.Attachment `json:"attachments,omitempty"`
+
+	// Root is the message that was broadcast to the channel when the SubType is
+	// thread_broadcast. If this is not a thread_broadcast message event, this
+	// value is nil.
+	Root *MessageEvent `json:"root"`
 }
 
 // MemberJoinedChannelEvent A member joined a public or private channel
