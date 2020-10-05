cb16760cd9960d187a5cf77ba3ca394fa7102406
Caleb Denio <cjdenio44@gmail.com>
2020-10-05 13:14:25 -0400

    Change event_context to eventContext
    Co-authored-by: Naoki Kanatani <k12naoki@gmail.com>

--- apps.go
+++ apps.go
@@ -19,7 +19,7 @@ type EventAuthorization struct {
 }
 
 // ListEventAuthorizations lists authed users and teams for the given event_context. You must provide an app-level token to the client using OptionAppLevelToken. More info: https://api.slack.com/methods/apps.event.authorizations.list
-func (api *Client) ListEventAuthorizations(event_context string) ([]EventAuthorization, error) {
+func (api *Client) ListEventAuthorizations(eventContext string) ([]EventAuthorization, error) {
 	resp := &listEventAuthorizationsResponse{}
 
 	err := api.postMethodWithBearerToken(context.Background(), "apps.event.authorizations.list", url.Values{
