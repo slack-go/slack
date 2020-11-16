f7284e29d867bc6322c80fe9fe21f02b0222c19e
Justin Judd <jbj@google.com>
2020-11-16 22:57:00 +0000

    Added all of the other possible entity types for completeness

--- audit.go
+++ audit.go
@@ -16,20 +16,18 @@ type AuditEntry struct {
 	DateCreate int    `json:"date_create"`
 	Action     string `json:"action"`
 	Actor      struct {
-		Type string `json:"type"`
-		User struct {
-			ID    string `json:"id"`
-			Name  string `json:"name"`
-			Email string `json:"email"`
-		} `json:"user"`
+		Type string    `json:"type"`
+		User AuditUser `json:"user"`
 	} `json:"actor"`
 	Entity struct {
 		Type string `json:"type"`
-		User struct {
-			ID    string `json:"id"`
-			Name  string `json:"name"`
-			Email string `json:"email"`
-		} `json:"user"`
+		// Only one of the below will be completed, based on the value of Type a user (W123AB456), a channel (C987XYZ654), a file (F42424242), an app (A27182818), a workspace (T31415926), or an enterprise (E1701NCCA)
+		User       AuditUser       `json:"user"`
+		Channel    AuditChannel    `json:"channel"`
+		File       AuditFile       `json:"file"`
+		App        AuditApp        `json:"app"`
+		Workspace  AuditWorkspace  `json:"workspace"`
+		Enterprise AuditEnterprise `json:"enterprise"`
 	} `json:"entity"`
 	Context struct {
 		Location struct {
@@ -41,6 +39,49 @@ type AuditEntry struct {
 		Ua        string `json:"ua"`
 		IPAddress string `json:"ip_address"`
 	} `json:"context"`
+}
+
+type AuditUser struct {
+	ID    string `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+	Team  string `json:"team"`
+}
+
+type AuditChannel struct {
+	ID          string `json:"id"`
+	Name        string `json:"name"`
+	Privacy     string `json:"privacy"`
+	IsShared    bool   `json:"is_shared"`
+	IsOrgShared bool   `json:"is_org_shared"`
+}
+
+type AuditFile struct {
+	ID       string `json:"id"`
+	Name     string `json:"name"`
+	Filetype string `json:"filetype"`
+	Title    string `json:"title"`
+}
+
+type AuditApp struct {
+	ID                  string   `json:"id"`
+	Name                string   `json:"name"`
+	IsDistributed       bool     `json:"is_distributed"`
+	IsDirectoryApproved bool     `json:"is_directory_approved"`
+	IsWorkflowApp       bool     `json:"is_workflow_app"`
+	Scopes              []string `json:"scopes"`
+}
+
+type AuditWorkspace struct {
+	ID     string `json:"id"`
+	Name   string `json:"name"`
+	Domain string `json:"domain"`
+}
+
+type AuditEnterprise struct {
+	ID     string `json:"id"`
+	Name   string `json:"name"`
+	Domain string `json:"domain"`
 }
 
 // AuditLogParameters contains all the parameters necessary (including the optional ones) for a GetAuditLogs() request
