e976c8a19225296f3f445ab5acd41629fd0ea2d7
xNok <nokwebspace@gmail.com>
2021-02-28 01:18:38 -0500

    fix: typos

--- socketmode/socketmode_eventhandler.go
+++ socketmode/socketmode_eventhandler.go
@@ -40,22 +40,22 @@ func NewsSocketmodeHandler(client *Client) *SocketmodeHandler {
 	}
 }
 
-// Register the middleare funtion to use to handle an Event (from socketmode)
+// Register the middleare function to use to handle an Event (from socketmode)
 func (r *SocketmodeHandler) Handle(et EventType, f SocketmodeHandlerFunc) {
 	r.EventMap[et] = append(r.EventMap[et], f)
 }
 
-// Register the middleare funtion to use to handle an Interaction
+// Register the middleare function to use to handle an Interaction
 func (r *SocketmodeHandler) HandleInteraction(et slack.InteractionType, f SocketmodeHandlerFunc) {
 	r.InteractionEventMap[et] = append(r.InteractionEventMap[et], f)
 }
 
-// Register the middleare funtion to use to handle an Event (from slackevents)
+// Register the middleare function to use to handle an Event (from slackevents)
 func (r *SocketmodeHandler) HandleEventsAPI(et slackevents.EventAPIType, f SocketmodeHandlerFunc) {
 	r.EventApiMap[et] = append(r.EventApiMap[et], f)
 }
 
-// Register the middleare funtion to use as a last resort
+// Register the middleare function to use as a last resort
 func (r *SocketmodeHandler) HandleDefault(f SocketmodeHandlerFunc) {
 	r.Default = f
 }
