eaac8eb94cf759ac250ef3cd36e0afbdd3232e47
Stephen Whitworth <stephenjameswhitworth@gmail.com>
2021-04-28 18:59:23 +0100

    Add underlying error messages

--- misc.go
+++ misc.go
@@ -29,6 +29,20 @@ type SlackResponse struct {
 	ResponseMetadata ResponseMetadata `json:"response_metadata"`
 }
 
+// SlackError contains both the error code, and associated messages.
+type SlackError struct {
+	Code     string
+	Messages []string
+}
+
+func (e SlackError) Error() string {
+	s := e.Code
+	if len(e.Messages) > 0 {
+		s = fmt.Sprintf("%s (%s)", s, strings.Join(e.Messages, ", "))
+	}
+	return s
+}
+
 func (t SlackResponse) Err() error {
 	if t.Ok {
 		return nil
@@ -41,7 +55,12 @@ func (t SlackResponse) Err() error {
 		return nil
 	}
 
-	return errors.New(t.Error)
+	err := SlackError{
+		Code:     t.Error,
+		Messages: t.ResponseMetadata.Messages,
+	}
+
+	return err
 }
 
 // RateLimitedError represents the rate limit respond from slack
