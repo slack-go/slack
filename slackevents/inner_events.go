1ba1d3f7f9b30585c5c61b5639f618b6c9cc9891
Peter Kristensen <peter@ptx.dk>
2021-07-17 13:54:15 +0200

    Fixed LinkSharedEvent and added context aware UnfurlMessage
    The MessageTimeStamp was incorrectly marked as a number. It should be
    a string as any other timestamp, and it required for Unfurl to work

--- slackevents/inner_events.go
+++ slackevents/inner_events.go
@@ -165,7 +165,7 @@ type LinkSharedEvent struct {
 	User             string        `json:"user"`
 	TimeStamp        string        `json:"ts"`
 	Channel          string        `json:"channel"`
-	MessageTimeStamp json.Number   `json:"message_ts"`
+	MessageTimeStamp string        `json:"message_ts"`
 	ThreadTimeStamp  string        `json:"thread_ts"`
 	Links            []sharedLinks `json:"links"`
 }
