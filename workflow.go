0a403b45318db13620ee30e2797a37e687112c1b
nghialv <nghialv2607@gmail.com>
2022-02-08 17:52:45 +0900

    Update workflow

--- workflow.go
+++ workflow.go
@@ -6,15 +6,21 @@ import (
 )
 
 type WorkflowUpdateRequest struct {
-	EditID  string                      `json:"workflow_step_edit_id"`
-	Inputs  *WorkflowStepInputRequest   `json:"inputs,omitempty"`
-	Outputs []WorkflowStepOutputRequest `json:"outputs,omitempty"`
+	EditID  string                       `json:"workflow_step_edit_id"`
+	Inputs  map[string]WorkflowStepInput `json:"inputs,omitempty"`
+	Outputs []WorkflowStepOutput         `json:"outputs,omitempty"`
 }
 
-type WorkflowStepInputRequest struct {
+type WorkflowStepInput struct {
+	Value                   string                 `json:"value,omitempty"`
+	SkipVariableReplacement bool                   `json:"skip_variable_replacement,omitempty"`
+	Variables               map[string]interface{} `json:"variables,omitempty"`
 }
 
-type WorkflowStepOutputRequest struct {
+type WorkflowStepOutput struct {
+	Name  string `json:"name,omitempty"`
+	Type  string `json:"type,omitempty"`
+	Label string `json:"label,omitempty"`
 }
 
 func (api *Client) UpdateWorkflow(ctx context.Context, req WorkflowUpdateRequest) (*SlackResponse, error) {
