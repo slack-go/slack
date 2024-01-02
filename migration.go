d5049da246a5faf5ba3c59dcb1c0b2152e409992
Tania Silva <persephone.was.taken@gmail.com>
2024-01-02 15:58:47 +0000

    Add ability to turn new enterprise grid IDs to old ones to migrationExchange

--- migration.go
+++ migration.go
@@ -7,6 +7,7 @@ import (
 
 type migrationExchangeResponseFull struct {
 	TeamID         string            `json:"team_id"`
+	ToOld          bool              `json:"to_old"`
 	EnterpriseID   string            `json:"enterprise_id"`
 	UserIDMap      map[string]string `json:"user_id_map"`
 	InvalidUserIDs []string          `json:"invalid_user_ids"`
@@ -14,13 +15,17 @@ type migrationExchangeResponseFull struct {
 }
 
 // MigrationExchange for Enterprise Grid workspaces, map local user IDs to global user IDs
-func (api *Client) MigrationExchange(ctx context.Context, teamID string, users []string) (map[string]string, error) {
+func (api *Client) MigrationExchange(ctx context.Context, teamID string, toOld bool, users []string) (map[string]string, error) {
 	values := url.Values{
 		"users": users,
 	}
 	if teamID != "" {
 		values.Add("team_id", teamID)
 	}
+	if toOld {
+		values.Add("to_old", "true")
+	}
+
 	response := &migrationExchangeResponseFull{}
 	err := api.getMethod(ctx, "migration.exchange", api.token, values, response)
 	if err != nil {
