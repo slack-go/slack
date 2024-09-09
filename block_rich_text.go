5fc79da666ea229823c2933ce35f68de2d22066e
YutoKashiwagi <kashiwagi19950714@gmail.com>
2024-09-09 17:52:11 +0900

    add unicode param in RichTextSectionEmojiElement

--- block_rich_text.go
+++ block_rich_text.go
@@ -341,6 +341,7 @@ type RichTextSectionEmojiElement struct {
 	Type     RichTextSectionElementType `json:"type"`
 	Name     string                     `json:"name"`
 	SkinTone int                        `json:"skin_tone"`
+	Unicode  string                     `json:"unicode,omitempty"`
 	Style    *RichTextSectionTextStyle  `json:"style,omitempty"`
 }
 
