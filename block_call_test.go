fdb5665c59456161463811a3a1d0568f307ad1cf
Sam Starling <42478+samstarling@users.noreply.github.com>
2024-12-07 18:44:36 +0000

    Update unit tests to assert on ID()

--- block_call_test.go
+++ block_call_test.go
@@ -8,6 +8,10 @@ import (
 
 func TestNewCallBlock(t *testing.T) {
 	callBlock := NewCallBlock("ACallID")
+
+	assert.Equal(t, callBlock.BlockType(), MBTCall)
 	assert.Equal(t, string(callBlock.Type), "call")
 	assert.Equal(t, callBlock.CallID, "ACallID")
+	assert.Equal(t, callBlock.BlockID, "")
+	assert.Equal(t, callBlock.ID(), "")
 }
--- block_section_test.go
+++ block_section_test.go
@@ -7,17 +7,17 @@ import (
 )
 
 func TestNewSectionBlock(t *testing.T) {
-
 	textInfo := NewTextBlockObject("mrkdwn", "*<fakeLink.toHotelPage.com|The Ritz-Carlton New Orleans>*\n★★★★★\n$340 per night\nRated: 9.1 - Excellent", false, false)
-
 	sectionBlock := NewSectionBlock(textInfo, nil, nil, SectionBlockOptionBlockID("test_block"))
+
+	assert.Equal(t, sectionBlock.BlockType(), MBTSection)
 	assert.Equal(t, string(sectionBlock.Type), "section")
 	assert.Equal(t, sectionBlock.BlockID, "test_block")
+	assert.Equal(t, sectionBlock.ID(), "test_block")
 	assert.Equal(t, len(sectionBlock.Fields), 0)
 	assert.Nil(t, sectionBlock.Accessory)
 	assert.Equal(t, sectionBlock.Text.Type, "mrkdwn")
 	assert.Contains(t, sectionBlock.Text.Text, "New Orleans")
-
 }
 
 func TestNewBlockSectionContainsAddedTextBlockAndAccessory(t *testing.T) {
